b302835ce4abdbd13a54ecaf54039a4302795b76
Dimitrij Denissenko <dimitrij.denissenko@blacksquaremedia.com>
2013-06-13 14:40:40 +0100

    Updated info spec

--- spec/ssdb_spec.rb
+++ spec/ssdb_spec.rb
@@ -45,7 +45,7 @@ describe SSDB do
     res["cmd.get"].keys.should =~ ["calls", "time_wait", "time_proc"]
 
     res["leveldb.stats"].should be_instance_of(Hash)
-    res["leveldb.stats"].should have(6).keys
+    res["leveldb.stats"].keys.should =~ ["compactions", "level", "read", "size", "time", "written"]
   end
 
   describe "scripting" do
